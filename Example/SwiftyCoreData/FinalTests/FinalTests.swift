df34590bb46a898039865e43e7361ec5c5c3f5ab
Ayush Goel <ayushgoel111@gmail.com>
2016-06-19 22:58:07 +0530

    Add tests for Final

--- Example/SwiftyCoreData/FinalTests/FinalTests.swift
+++ Example/SwiftyCoreData/FinalTests/FinalTests.swift
@@ -7,7 +7,27 @@
 //
 
 import XCTest
+import CoreData
 @testable import Final
 
 class FinalTests: XCTestCase {
+  func testEntityName() {
+    XCTAssertEqual("Task", Task.name())
+  }
+
+  func testObjectSavedIsReturned() {
+    let moc = testContext()
+    guard let task = NSEntityDescription.insertNewObjectForEntityForName(Task.name(), inManagedObjectContext: moc) as? Task else {
+      assertionFailure()
+      return
+    }
+    task.id = NSNumber(unsignedInt: arc4random())
+    moc.saveContext()
+
+    guard let retrievedTask = Task.object(moc, predicate: nil) else {
+      assertionFailure()
+      return
+    }
+    XCTAssertEqual(retrievedTask.id, task.id)
+  }
 }
