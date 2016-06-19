86aa6d2199ac4652d59f1424dabbc25c20f1d321
Ayush Goel <ayushgoel111@gmail.com>
2016-06-19 20:01:42 +0530

    Add target Initial

---
+++ Example/SwiftyCoreData/Initial/Task.swift
@@ -0,0 +1,17 @@
+//
+//  Task.swift
+//  SwiftyCoreData
+//
+//  Created by Ayush Goel on 19/06/16.
+//  Copyright © 2016 Ayush Goel. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Task: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
---
+++ Example/SwiftyCoreData/InitialTests/InitialTests.swift
@@ -0,0 +1,14 @@
+//
+//  InitialTests.swift
+//  InitialTests
+//
+//  Created by Ayush Goel on 19/06/16.
+//  Copyright © 2016 Ayush Goel. All rights reserved.
+//
+
+import XCTest
+@testable import Initial
+
+class InitialTests: XCTestCase {
+
+}
